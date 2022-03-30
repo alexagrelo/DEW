debadbf7c98994cc6a834b0be5f7d544f370d232
Alex Agrelo <alex.agrelo@gmail.com>
2022-03-30 13:48:47 +0100

    activ 4 funcional

---
+++ Actividad 4/scripts.js
@@ -0,0 +1,46 @@
+// Imagen que queremos comprobar si está en el servidor:
+const urlmagenAComprobar = 'changed.png';
+
+// Usando XMLHttpRequest y jQuery
+// Documentación XMLHttpRequest:
+// https://developer.mozilla.org/es/docs/Web/API/XMLHttpRequest
+// https://developer.mozilla.org/es/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
+// Documentación jQuery:
+// https://www.anerbarrena.com/jquery-selectors-selectores-4768/
+
+// $(document).ready(() => {
+//     let req = new XMLHttpRequest();
+//     // let changedPngExists = false;
+//     req.open('GET', 'img/' + urlImagenAComprobar, true);
+//     // req.open('GET', 'img/404_img.png', true);
+//     req.onreadystatechange = (aEvt) => {
+//         if (req.readyState == 4) {
+//             if (req.status == 200) {
+//                 $('#img-changed').attr('src', 'img/changed.png');
+//                 console.log('changed.png existe');
+//             } else {
+//                 $(".modal").modal("show");
+//                 console.warn('changed.png no existe en el servidor');
+//             }
+//         }
+//     };
+//     req.send(null);
+// });
+
+// USANDO AJAX
+$(document).ready(() => {
+    $.ajax({
+        url: 'img/' + urlmagenAComprobar,
+        type: 'HEAD',
+        error: () => {
+            $(".modal").modal("show");
+            console.warn(urlmagenAComprobar, 'no existe en el servidor');
+        },
+        success: () => {
+            $('#img-changed').attr('src', 'img/changed.png');
+            console.log(urlmagenAComprobar, 'existe');
+        }
+    });
+})
+
+
--- DAW_DEW_Ampliac_2/main.js
+++ DAW_DEW_Ampliac_2/main.js
@@ -1,1 +1,60 @@
 //write your JS code here:
+const sum = (items) => {
+    const divTarget = document.getElementById('sum');
+    divTarget.innerHTML = items.reduce((a, b) => a + b, 0);
+    
+    // let result = 0;
+    // items.forEach(element => {
+    //     result += element;
+    // });
+    // return result;
+}
+
+const multiplication = (items) => {
+    let result = 0;
+    items.forEach(element => {
+        result *= element;
+    });
+    return result;
+}
+
+const add = (items) => {
+    let result = [];
+    items.forEach(element => {
+        (element.isArray) ? element.forEach(e => result.push(e)) : result.push(element);
+    });
+    return `[${result.join(', ')}]`;
+
+}
+
+const extract = (items, term) => {
+    let income = [];
+    items.forEach(element => {
+        (element.isArray) ? element.forEach(e => income.push(e)) : income.push(element);
+    });
+    result = income.filter( element => element !== term );
+    return `[${result.join(', ')}]`;
+}
+
+const merge = (items) => {
+    let result = [];
+    items.forEach(element => result = result.concat(element));
+    return `[${result.join(', ')}]`;
+}
+
+const concat = (items) => {
+    let result = '';
+    items.forEach(element => result += element);
+    return result;
+}
+
+const sort = (items) => {
+    let result = items.sort((a, b) => a - b);
+    return `[${result.join(', ')}]`;
+}
+
+
+
+const divResults = document.getElementById('results');
+
+
