a543283fcbbfd69927d7f4f63077bf035bb93e56
Alex Agrelo <alex.agrelo@gmail.com>
2022-04-27 14:43:45 +0100

    reestructuración de archivos

--- DAW_DEW_Actividad6/scripts/scripts.js
+++ DAW_DEW_Actividad6/scripts/scripts.js
@@ -5,10 +5,10 @@ const footerDiv = $("#footer");
 const urlParams = new URLSearchParams(window.location.search);
 const content = urlParams.get("content");
 
-headerDiv.load("header.html", function (responseText, statusText, xhr) {
+headerDiv.load("../components/header.html", function (responseText, statusText, xhr) {
     if (statusText === "success") {
         // console.log('responseText', responseText);
-        // console.log("Header loaded");
+        console.log("Header loaded");
         const page_1 = $("#page_1");
         const page_2 = $("#page_2");
         const page_3 = $("#page_3");
@@ -16,7 +16,7 @@ headerDiv.load("header.html", function (responseText, statusText, xhr) {
             case "page_1":
                 page_1.addClass("active disabled");
                 page_1.attr("disabled", true);
-                mainDiv.load("content_1.html", function (responseText, statusText, xhr) {
+                mainDiv.load("pages/content_1.html", function (responseText, statusText, xhr) {
                     if (statusText === "success") {
                         console.log('Page 1 loaded');
                     } else if (statusText === "error") {
@@ -29,7 +29,7 @@ headerDiv.load("header.html", function (responseText, statusText, xhr) {
             case "page_2":
                 page_2.addClass("active disabled");
                 page_2.attr("disabled", true);
-                mainDiv.load("content_2.html", function (responseText, statusText, xhr) {
+                mainDiv.load("pages/content_2.html", function (responseText, statusText, xhr) {
                     if (statusText === "success") {
                         console.log('Page 2 loaded');
                     } else if (statusText === "error") {
@@ -42,7 +42,7 @@ headerDiv.load("header.html", function (responseText, statusText, xhr) {
             case "page_3":
                 page_3.addClass("active disabled");
                 page_3.attr("disabled", true);
-                mainDiv.load("content_3.html", function (responseText, statusText, xhr) {
+                mainDiv.load("pages/content_3.html", function (responseText, statusText, xhr) {
                     if (statusText === "success") {
                         console.log('Page 3 loaded');
                     } else if (statusText === "error") {
@@ -55,6 +55,15 @@ headerDiv.load("header.html", function (responseText, statusText, xhr) {
             default:
                 page_1.addClass("active disabled");
                 page_1.attr("disabled", true);
+                mainDiv.load("pages/content_1.html", function (responseText, statusText, xhr) {
+                    if (statusText === "success") {
+                        console.log('Page 1 loaded');
+                    } else if (statusText === "error") {
+                        console.log("Error: " + xhr.status + ": " + xhr.statusText);
+                    } else {
+                        console.log("Unknown error");
+                    }
+                });
                 break;
         }
     } else if(statusText === "error") {
@@ -65,4 +74,12 @@ headerDiv.load("header.html", function (responseText, statusText, xhr) {
 });
 
 
-footerDiv.load("footer.html");
+footerDiv.load("../components/footer.html", function (responseText, statusText, xhr) {
+    if (statusText === "success") {
+        console.log('Footer loaded');
+    } else if (statusText === "error") {
+        console.log("Error: " + xhr.status + ": " + xhr.statusText);
+    } else {
+        console.log("Unknown error");
+    }
+});
