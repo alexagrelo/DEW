6374080ed7ff18d3f6c30f41f333fe632b74d188
Alex Agrelo <alex.agrelo@gmail.com>
2022-04-27 11:58:33 +0100

    act 6 funcional

--- DAW_DEW_Actividad6/scripts.js
+++ DAW_DEW_Actividad6/scripts.js
@@ -5,54 +5,64 @@ const footerDiv = $("#footer");
 const urlParams = new URLSearchParams(window.location.search);
 const content = urlParams.get("content");
 
-headerDiv.load("header.html", function(responseText, statusText, xhr) {
-  if (statusText === "success") {
-    console.log("Header loaded");
-    const page_1 = $("#page_1");
-    const page_2 = $("#page_2");
-    const page_3 = $("#page_3");
-    switch(content) {
-        case "page_1":
-            page_1.addClass("active disabled");
-            page_1.attr("disabled", true);
-            break;
-        case "page_2":
-            page_2.addClass("active disabled");
-            page_2.attr("disabled", true);
-            break;
-        case "page_3":
-            page_3.addClass("active disabled");
-            page_3.attr("disabled", true);
-            break;
-        default:
-            page_1.addClass("active disabled");
-            page_1.attr("disabled", true);
-            break;
+headerDiv.load("header.html", function (responseText, statusText, xhr) {
+    if (statusText === "success") {
+        // console.log('responseText', responseText);
+        // console.log("Header loaded");
+        const page_1 = $("#page_1");
+        const page_2 = $("#page_2");
+        const page_3 = $("#page_3");
+        switch (content) {
+            case "page_1":
+                page_1.addClass("active disabled");
+                page_1.attr("disabled", true);
+                mainDiv.load("content_1.html", function (responseText, statusText, xhr) {
+                    if (statusText === "success") {
+                        console.log('Page 1 loaded');
+                    } else if (statusText === "error") {
+                        console.log("Error: " + xhr.status + ": " + xhr.statusText);
+                    } else {
+                        console.log("Unknown error");
+                    }
+                });
+                break;
+            case "page_2":
+                page_2.addClass("active disabled");
+                page_2.attr("disabled", true);
+                mainDiv.load("content_2.html", function (responseText, statusText, xhr) {
+                    if (statusText === "success") {
+                        console.log('Page 2 loaded');
+                    } else if (statusText === "error") {
+                        console.log("Error: " + xhr.status + ": " + xhr.statusText);
+                    } else {
+                        console.log("Unknown error");
+                    }
+                });
+                break;
+            case "page_3":
+                page_3.addClass("active disabled");
+                page_3.attr("disabled", true);
+                mainDiv.load("content_3.html", function (responseText, statusText, xhr) {
+                    if (statusText === "success") {
+                        console.log('Page 3 loaded');
+                    } else if (statusText === "error") {
+                        console.log("Error: " + xhr.status + ": " + xhr.statusText);
+                    } else {
+                        console.log("Unknown error");
+                    }
+                });
+                break;
+            default:
+                page_1.addClass("active disabled");
+                page_1.attr("disabled", true);
+                break;
+        }
+    } else if(statusText === "error") {
+        console.error("Error: " + xhr.status + " " + xhr.statusText);
+    } else {
+        console.error("Unknown error");
     }
-
-
-  }
 });
 
-switch (content) {
-    case "page_1":
-        console.log("page_1");
-        console.log(headerDiv.textContent);
-        // document.getElementById("page_1").classList.add("active");
-        // document.getElementById("page_2").classList.remove("active");
-        // document.getElementById("page_3").classList.remove("active");
-        mainDiv.load("content_1.html");
-        break;
-    case "page_2":
-        console.log("page_2");
-        mainDiv.load("content_2.html");
-        break;
-    case "page_3":
-        console.log("page_3");
-        mainDiv.load("content_3.html");
-        break;
-    default:
-        mainDiv.load("content_1.html");
-    }
 
 footerDiv.load("footer.html");
